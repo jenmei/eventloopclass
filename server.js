feefb828aa7a6601755e620dfbd466b93563a690
macournoyer <macournoyer@gmail.com>
2012-09-13 14:49:23 -0400

    Using non-blocking IO and select in server.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var syscalls = require('syscalls');
 
 var fd = syscalls.socket(syscalls.AF_INET, syscalls.SOCK_STREAM, 0);
+syscalls.fcntl(fd, syscalls.F_SETFL, syscalls.O_NONBLOCK);
 
 syscalls.bind(fd, 3000, "0.0.0.0");
 
@@ -9,12 +10,15 @@ syscalls.listen(fd, 100);
 console.log("Listening on port 3000");
 
 while (true) {
+  syscalls.select([fd], [], [], 0);
   var connFd = syscalls.accept(fd);
   console.log("Accepted new connection");
   
+  syscalls.select([connFd], [], [], 0);
   var data = syscalls.read(connFd, 1024);
   console.log("Received: " + data);
-
+  
+  syscalls.select([], [connFd], [], 0);
   syscalls.write(connFd, "bye!\n");
 
   syscalls.close(connFd);
