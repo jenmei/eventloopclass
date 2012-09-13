58e817893b3df77c7a2596754475ef579aea7a44
Jen-Mei Wu <jenmei@colorxp.com>
2012-09-13 12:25:33 -0700

    use non-blocking calls

--- client.js
+++ client.js
@@ -2,9 +2,13 @@ var syscalls = require('syscalls');
 
 var fd = syscalls.socket(syscalls.AF_INET, syscalls.SOCK_STREAM, 0);
 
+syscalls.fcntl(fd, syscalls.F_SETFL, syscalls.O_NONBLOCK);
+
 syscalls.connect(fd, 3000, "0.0.0.0");
 
+syscalls.select([], [fd], [], 0);
 syscalls.write(fd, "hi\n");
 
+syscalls.select([fd], [], [], 0);
 var data = syscalls.read(fd, 1024);
 console.log("Server sent: " + data);
