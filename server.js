7191c165829af1d11f4335ba2358652c03ced3cb
Jen-Mei Wu <jenmei@colorxp.com>
2012-09-13 12:45:17 -0700

    added concurrency to server

--- server.js
+++ server.js
@@ -9,17 +9,49 @@ syscalls.listen(fd, 100);
 
 console.log("Listening on port 3000");
 
-while (true) {
-  syscalls.select([fd], [], [], 0);
-  var connFd = syscalls.accept(fd);
-  console.log("Accepted new connection");
-  
-  syscalls.select([connFd], [], [], 0);
-  var data = syscalls.read(connFd, 1024);
-  console.log("Received: " + data);
-  
-  syscalls.select([], [connFd], [], 0);
-  syscalls.write(connFd, "bye!\n");
+var readables = {}; // {fd: callback}
+var writables = {}; // {fd: callback}
 
-  syscalls.close(connFd);
+readables[fd] = function () {
+  var connFd = syscalls.accept(fd);
+  syscalls.fcntl(connFd, syscalls.F_SETFL, syscalls.O_NONBLOCK);
+  console.log("Accepted new connection");
+
+  readables[connFd] = function () {
+    var data = syscalls.read(connFd, 1024);
+    console.log("Received: " + data);
+    delete readables[connFd];
+
+    writables[connFd] = function () {
+      syscalls.write(connFd, "bye!\n");
+      syscalls.close(connFd);
+      delete writeables[connFd];
+    }
+  }
+
 }
+
+while (true) {
+  // fds = [[readables], [writables], [errors]];
+  var fds = syscalls.select(Object.keys(readables), Object.keys(writables), [], 0);
+
+  var readableFds = fds[0];
+  var writableFds = fds[1];
+
+  for (var i=0; i< readableFds.length; i++) {
+    var fd = readableFds[i];
+    var callback = readables[fd];
+    callback();
+  }
+
+  for (var i=0; i< writables.length; i++) {
+    var fd = writableFds[i];
+    var callback = writables[fd];
+    callback();
+  }
+}
+
+
+
+
+
